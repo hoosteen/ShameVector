f98d3e43e7ac4bdebd30fdf1d34586b8af4d5ba8
hoosteen <justinsmith1986@gmail.com>
2015-02-07 22:31:49 -0500

    OAuthFactory Code Snippet
    Attempt at OAuth authentication via sample code in "R for Social Media
    Analysis"

---
+++ authentication_code.r
@@ -0,0 +1,10 @@
+consumerKey <- API_key
+consumerSecret <- API_secret
+requestURL='https://api.twitter.com/oauth/request_token'
+accessURL='https://api.twitter.com/oauth/access_token'
+authURL='https://api.twitter.com/oauth/authorize'
+cred <- OAuthFactory$new(consumerKey=consumerKey,
+                         consumerSecret=consumerSecret,
+                         requestURL=reqURL,
+                         accessURL=accessURL,
+                         authURL=authURL)
